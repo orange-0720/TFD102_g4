e476980dbbad61ec8157360b4f758b9b79319fc2
orange-0720 <orange0720@hotmail.com>
2021-07-21 20:22:43 +0800

    7/21 修改會員登入頁面 新增手機板header 新增購物車彈窗 新增聯絡我們彈窗

---
+++ src/assets/js/header.js
@@ -0,0 +1,43 @@
+
+// 固定header
+var menu = document.getElementsByClassName("menu")[0];
+document.addEventListener("scroll", function(){
+  console.log(window.scrollY);
+  // console.log(menu)
+  // console.log(menu_top);
+  console.log(menu.style.top)
+  menu.style.top = (0 - (scrollY)) + "px";
+});
+
+// 漢堡列表展開
+$('.ham').on("click",function(){
+  if(window.innerWidth > 1200){
+    $('.jump_hamburger_block').fadeToggle();
+  }else{
+    $('.jump_hamburger_block').toggleClass('ham_block_move')
+  }
+  $('.ham_top').toggleClass('ham_top_move');
+  $('.ham_middle').toggleClass('ham_middle_move')
+  $('.ham_bottom').toggleClass('ham_bottom_move');
+})
+
+// header 聯絡我們
+$('.comm_us').on('click', function(){
+  $('.index_contact_box').fadeToggle()
+});
+
+$('.nav_block_contact').on('click', function(){
+  $('.index_contact_box').fadeToggle()
+});
+
+$('.contact_block_Xmark').click(function(){
+  $('.index_contact_box').fadeToggle()
+});
+
+$('.black_block').click(function(){
+  $('.index_contact_box').fadeToggle();
+});
+
+$('.shop_car').click(function(){
+  $('.header_shopping_cart').toggleClass('cart_into');
+})
