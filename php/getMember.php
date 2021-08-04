bd3963fe60bc3ac582195edaf1a3bb023ec92586
orange-0720 <orange0720@hotmail.com>
2021-08-04 17:06:18 +0800

    0804 新增多個php 購物車寫到一半， 會員頁面差訂單串接及留言板串接

--- php/getMember.php
+++ php/getMember.php
@@ -5,14 +5,13 @@
 
     //建立SQL語法
 
-
     include("./Lib/Member.php");
     getMemberID();
     $memberID = $_SESSION['MemberID'];
 
     // 從$_SETTION抓出顧客ID，透過顧客ID撈出此顧客的活動預約。
     // $sql = "SELECT * FROM `tfd102-g4`.APPOINTMENT WHERE CUSTOMER_ID = $_SESSION(memberID)";
-    $sql = "SELECT * FROM `tfd102-g4`.APPOINTMENT WHERE CUSTOMER_ID = $memberID";
+    $sql = "SELECT * FROM `tfd102-g4`.CUSTOMER WHERE CUSTOMER_ID = $memberID";
 
 
 
